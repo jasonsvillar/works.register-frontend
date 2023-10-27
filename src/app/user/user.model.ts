53928aec56f167cd0860ee4b7d032a7f12a908f8
jasonsvillar <jason.villar633@gmail.com>
2023-10-27 17:23:48 -0300

    User model WIP

--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -1,2 +1,19 @@
 export class User {
+  private id: number;
+  private name: string;
+  private email: string;
+
+  constructor(id: number, name: string, email: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
 }
