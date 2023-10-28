598f6ced48da74643e3bddf8f8b970e7fcdcae14
jasonsvillar <jason.villar633@gmail.com>
2023-10-28 16:53:48 -0300

    Comment unused line

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { UserModel } from '../user/user.model';
+//import { UserModel } from '../user/user.model';
 
 const baseUrl = 'http://localhost:8080/api/v1/user';
 
