752d698c01b55ac781b0bba43d32345ea05dbb5e
jasonsvillar <jason.villar633@gmail.com>
2023-10-29 23:34:53 -0300

    New header and new font reydex

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily :{ 
+        reydex: ["reydex","sans-serif"],
+      }
+    },
   },
   plugins: []
 }
