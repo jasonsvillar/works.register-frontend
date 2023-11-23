0eccf8a0e7323bbef99fe78be0199e6b3654bea7
jasonsvillar <jason.villar633@gmail.com>
2023-11-22 22:51:16 -0300

    Change url of render dev frontend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     isProduction: true,
-    link:'https://works-register.onrender.com',
+    link:'https://works-register-dev.onrender.com',
     linkBackend: 'https://works-api-rest-dev.onrender.com'
 };
